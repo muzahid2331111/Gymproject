32ff6bf0977e1c79bb37e846440291015c050a67
nisa-17 <2312217@iub.edu.bd>
2025-08-16 02:26:00 +0600

    6th commit

--- gymSIM/src/main/java/Dietetian/TrackProgress.java
+++ gymSIM/src/main/java/Dietetian/TrackProgress.java
@@ -3,15 +3,17 @@ package Dietetian;
 import java.time.LocalDate;
 
 public class TrackProgress {
-    private String memberID ;
-    private int height ; //cm
-    private int weight; //kg
-    LocalDate date ;
+    private String memberID;
+    private double height; //cm
+    private double weight; //kg
+    private double bmi;
+    LocalDate date;
 
-    public TrackProgress(String memberID, int height, int weight, LocalDate date) {
+    public TrackProgress(String memberID, double height, double weight, double bmi, LocalDate date) {
         this.memberID = memberID;
         this.height = height;
         this.weight = weight;
+        this.bmi = bmi;
         this.date = date;
     }
 
@@ -23,20 +25,28 @@ public class TrackProgress {
         this.memberID = memberID;
     }
 
-    public int getHeight() {
+    public double getHeight() {
         return height;
     }
 
-    public void setHeight(int height) {
+    public void setHeight(double height) {
         this.height = height;
     }
 
-    public int getWeight() {
+    public double getWeight() {
         return weight;
     }
 
-    public void setWeight(int weight) {
+    public void setWeight(double weight) {
         this.weight = weight;
+    }
+
+    public double getBmi() {
+        return bmi;
+    }
+
+    public void setBmi(double bmi) {
+        this.bmi = bmi;
     }
 
     public LocalDate getDate() {
