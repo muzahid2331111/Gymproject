611566e703d81ed82794c2c06eff41a6fbeaea9f
muzahid2331111 <2331111@iub.edu.bd>
2025-08-15 13:54:33 -0700

    save work before pull

---
+++ gymSIM/src/main/java/Trainer/DesignPersonalizedNutritionPlans.java
@@ -0,0 +1,26 @@
+package Trainer;
+
+public class DesignPersonalizedNutritionPlans
+{
+    @javafx.fxml.FXML
+    private TextField fatsField;
+    @javafx.fxml.FXML
+    private TextField carbsField;
+    @javafx.fxml.FXML
+    private TextField caloriesField;
+    @javafx.fxml.FXML
+    private TextField clientNameField;
+    @javafx.fxml.FXML
+    private TextField proteinField;
+    @javafx.fxml.FXML
+    private TextArea restrictionsField;
+    @javafx.fxml.FXML
+    private ComboBox goalDropdown;
+    @javafx.fxml.FXML
+    private TextArea mealPlanField;
+    @javafx.fxml.FXML
+    private Button saveButton;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
---
+++ gymSIM/src/main/java/Trainer/TrackClientProgressAndGenerateReports.java
@@ -0,0 +1,32 @@
+package Trainer;
+
+public class TrackClientProgressAndGenerateReports
+{
+    @javafx.fxml.FXML
+    private ProgressBar nutritionAdherenceBar;
+    @javafx.fxml.FXML
+    private CheckBox includeGoalsCheck;
+    @javafx.fxml.FXML
+    private Button btnDownload;
+    @javafx.fxml.FXML
+    private TextArea notesArea;
+    @javafx.fxml.FXML
+    private ProgressBar workoutCompletionBar;
+    @javafx.fxml.FXML
+    private ProgressBar strengthBar;
+    @javafx.fxml.FXML
+    private ProgressBar enduranceBar;
+    @javafx.fxml.FXML
+    private ComboBox timeframeSelector;
+    @javafx.fxml.FXML
+    private CheckBox includeTrendChartsCheck;
+    @javafx.fxml.FXML
+    private Button btnGenerate;
+    @javafx.fxml.FXML
+    private ComboBox clientSelector;
+    @javafx.fxml.FXML
+    private Button btnSchedule;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
